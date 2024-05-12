b3ee6b9a5824d020887f8ebf7897e5dc40e644c2
Pranav Bawgikar <102728016+PranavBawgikar@users.noreply.github.com>
2024-05-12 13:39:57 +0530

    Add files via upload

---
+++ cyberpunk-3100-main/app/head.js
@@ -0,0 +1,9 @@
+const Head = () => (
+  <>
+    <title>Cyberpunk 3100</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/favicon.ico" />
+  </>
+);
+
+export default Head;
